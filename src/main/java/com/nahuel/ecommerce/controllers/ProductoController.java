31a044c31c2a804255950e5eb074ab3dca358804
Nahuel Perea <nahuel.perea.dev@gmail.com>
2026-07-02 16:07:04 -0300

    creacion de metodo en ProductoController para crear entidad producto, funcionando

--- src/main/java/com/nahuel/ecommerce/controllers/ProductoController.java
+++ src/main/java/com/nahuel/ecommerce/controllers/ProductoController.java
@@ -1,7 +1,12 @@
 package com.nahuel.ecommerce.controllers;
 
+import com.nahuel.ecommerce.dtos.ProductoDto;
 import com.nahuel.ecommerce.services.ProductoServiceImp;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class ProductoController {
 
     private final ProductoServiceImp productoService;
 
+    @PostMapping
+    public ResponseEntity<?> crearProducto(@RequestBody ProductoDto dto){
+        ProductoDto ndto=productoService.guardarProducto(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ndto);
+    }
 
 }
