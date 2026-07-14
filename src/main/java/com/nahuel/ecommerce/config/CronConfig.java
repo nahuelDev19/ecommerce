e67dd739f83057e962324f2b46cc02574bb8f3f9
Nahuel Perea <nahuel.perea.dev@gmail.com>
2026-07-13 23:02:05 -0300

    config basica de cron

--- src/main/java/com/nahuel/ecommerce/config/CronConfig.java
+++ src/main/java/com/nahuel/ecommerce/config/CronConfig.java
@@ -1,11 +1,51 @@
 package com.nahuel.ecommerce.config;
 
+import com.nahuel.ecommerce.entitys.Carrito;
+import com.nahuel.ecommerce.repositories.CarritoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CronConfig {
+
+    private final CarritoRepository carritoRepository;
+
+
+    @Scheduled(cron = "0 */2 * * * *")
+    public void recordatorioCarritoAbandonado(){
+
+        List<Carrito> carritos= carritoRepository.findCarritoUltimaInteraccion(Instant.now().minusSeconds(10*60));
+        if (carritos.isEmpty()) {
+            log.info("Job de limpieza finalizado: no se encontraron carritos");
+            return;
+        }
+
+        carritos.forEach(carrito -> {
+            try {
+                System.out.println(
+                        "hola "+
+                        carrito.getUsuario().getId() + "dejaste abandonado tu carrito."
+                );
+                //log.debug("Producto eliminado - id={}, nombre={}", producto.getId(), producto.getNombre());
+            } catch (Exception e) {
+                //log.error("Error al eliminar producto id={}, nombre={}", producto.getId(), producto.getNombre(), e);
+            }
+        });
+
+        //log.info("Job de limpieza finalizado: {} carritos", carritos.size());
+
+
+    }
+
+
 }
--- src/main/java/com/nahuel/ecommerce/repositories/CarritoRepository.java
+++ src/main/java/com/nahuel/ecommerce/repositories/CarritoRepository.java
@@ -4,7 +4,12 @@ import com.nahuel.ecommerce.entitys.Carrito;
 import com.nahuel.ecommerce.entitys.EstadoCarrito;
 import com.nahuel.ecommerce.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,5 +17,11 @@ public interface CarritoRepository extends JpaRepository<Carrito, UUID> {
 
     Optional<Carrito> findByUsuarioIdAndEstadoCarrito(UUID usuarioId, EstadoCarrito estado);
 
+    @Query("""
+            SELECT c FROM Carrito c WHERE c.ultimaInteraccion <= :ultimaInteraccion
+            """)
+    List<Carrito> findCarritoUltimaInteraccion(
+            @Param("ultimaInteraccion") Instant ultimaInteraccion
+    );
 
 }
